b19be673c2e0cb8df7476041304554f3db914da7
andriidotsenko <andrui.dots@gmail.com>
2023-07-20 23:49:26 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,39 @@
+// 1. Задача про обчислення різниці часу
+//=========================================================================================
+// Напишіть функцію яка буде буде приймати 3 параметри
+// — початкову дату (string)
+// — кінцеву дату (string)
+// — розмірність ('days', 'hours', 'minutes', seconds)
+
+// Функція повертатиме часовий період між цими датами згідно розмірності.
+// Також вкажіть значення по замовчуванню для всіх цих параметрів (на ваш вибір).
+// Функція має коректно працювати навіть якщо початкова дата пізніше ніж кінцева дата.
+
+// Приклади:
+// durationBetweenDates('02 Aug 1985', '03 Aug 1985', 'seconds')  // поверне '86400 seconds'
+// durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')  // поверне '362 days'
+//==========================================================================================
+
+function durationBetweenDates(strOne, strTwo, preset) {
+
+	const dateOne = new Date(strOne)
+	const dateTwo = new Date(strTwo)
+
+	if (isNaN(dateOne - dateTwo)) {
+		return `Invalid date inputs (1 or 2 args)!`
+	} else if (preset === 'days') {
+		return ` ${Math.abs((dateOne - dateTwo) / 1000 / 60 / 60 / 24)} ${(Math.abs((dateOne - dateTwo) / 1000 / 60 / 60 / 24)) > 1 ? "days" : "day"}`
+	} else if (preset === 'hours') {
+		return ` ${Math.abs((dateOne - dateTwo) / 1000 / 60 / 60)} ${(Math.abs((dateOne - dateTwo) / 1000 / 60 / 60)) > 1 ? "hours" : "hour"}`
+	} else if (preset === 'minutes') {
+		return ` ${Math.abs((dateOne - dateTwo) / 1000 / 60)} ${(Math.abs((dateOne - dateTwo) / 1000 / 60)) > 1 ? "minutes" : "minute"
+			}`
+	} else if (preset === 'seconds') {
+		return ` ${Math.abs((dateOne - dateTwo) / 1000)} ${(Math.abs((dateOne - dateTwo) / 1000)) > 1 ? "seconds" : "second"}`
+	} else {
+		return `Invalid preset (3arg)!`
+	}
+}
+console.log(durationBetweenDates('2 Feb 2025', '1 Feb 2025', 'months'))
+
+//========================================================================================================================================================
